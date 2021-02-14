d32e0c4d50f24208741fed64316dfc5343d10eef
Andy <andriusb@ethz.ch>
2021-02-14 19:00:50 +0100

    Changing structure to add PyHDFE in the name

--- test_clustering.py
+++ test_clustering.py
@@ -1,14 +1,15 @@
-import FixedEffectModel.api as FEM
+import FixedEffectModelPyHDFE.api as FEM
 import pandas as pd
 import numpy as np
 
 df = pd.read_stata('/home/abom/Desktop/regPyHDFE/data/test.dta')
 consist_input = ['wks_ue','tenure']
 output_input = ['ttl_exp']
-category_input = ['year', 'idcode']
-cluster_input = ['year']
+category_input = ['idcode']
+cluster_input = ['fifty_clusts']
+
 print("absorbed", category_input)
 print("clustered", cluster_input)
-result1 = FEM.ols_high_d_category(df,consist_input,output_input,category_input,cluster_input,formula=None,robust=False,c_method = 'cgm',epsilon = 1e-8,max_iter = 1e6)
+result1 = FEM.ols_high_d_category(df,consist_input,output_input,category_input,cluster_input,formula=None,robust=False,epsilon = 1e-8,max_iter = 1e6)
  
 result1.summary()
